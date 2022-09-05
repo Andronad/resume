b41fec54f23c9ce685e6584ec9dd1e6bf3c8db61
Andronad <andronad.am@gmail.com>
2022-09-05 13:03:03 +0300

    add responsive menu

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,30 +1,102 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+    AppBar,
+    Box,
+    IconButton,
+    Menu,
+    MenuItem,
+    Toolbar,
+    Typography,
+} from "@mui/material";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { TowLabelSwitch } from "../common/TwoLabelSwitch/TwoLabelSwitch";
 import VerticalDivider from "../common/VerticalDivider";
 import { changeThemeMode } from "./../../store/actionCreators/index";
 import { StyledLink } from "./../common/StyledLink/StyledLink";
 import "./Navbar.scss";
 
+const pages = [
+    { title: "ANDREI MINZER CV", route: "/" },
+    { title: "TODO", route: "/todo" },
+    { title: "XO", route: "/tictactoe" },
+];
+
 export const Navbar = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [anchorElNav, setAnchorElNav] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = (pageRoute) => {
+        setAnchorElNav(null);
+        navigate(pageRoute);
+    };
+
     return (
         <AppBar position="sticky">
             <Toolbar>
+                <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                    <IconButton
+                        size="large"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleOpenNavMenu}
+                        color="inherit"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                            vertical: "bottom",
+                            horizontal: "left",
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: "top",
+                            horizontal: "left",
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                        sx={{
+                            display: { xs: "block", md: "none" },
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <MenuItem
+                                key={page.title}
+                                onClick={() => handleCloseNavMenu(page.route)}
+                            >
+                                <Typography textAlign="center">
+                                    {page.title}
+                                </Typography>
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                </Box>
                 <Box
-                    sx={{ display: "flex", flexDirection: "row", flexGrow: 1 }}
+                    sx={{
+                        display: { md: "flex", xs: "none" },
+                        flexDirection: "row",
+                        flexGrow: 1,
+                    }}
                 >
-                    <StyledLink to="/">
-                        <Typography variant="h4">ANDREI MINZER CV</Typography>
-                    </StyledLink>
-                    <VerticalDivider />
-                    <StyledLink to="/todo">
-                        <Typography variant="h4">TODO</Typography>
-                    </StyledLink>
-                    <VerticalDivider />
-                    <StyledLink to="/tictactoe">
-                        <Typography variant="h4">X/O</Typography>
-                    </StyledLink>
+                    {pages.map((page) => (
+                        <>
+                            <StyledLink to={page.route} key={page.title}>
+                                <Typography variant="h4">
+                                    {page.title}
+                                </Typography>
+                            </StyledLink>
+                            <VerticalDivider />
+                        </>
+                    ))}
                 </Box>
                 <TowLabelSwitch
                     label1="Light"
